7d9cc60946e9b3dcd900239f88b6b943868d9f28
Salim Al Mersally <salim.almersally@lau.edu>
2022-03-15 18:04:37 +0200

    Fixed files structure

--- helper/costCalc.py
+++ helper/costCalc.py
@@ -1,49 +1,34 @@
-from operator import truediv
-from tarfile import ENCODING
 import xml.etree.ElementTree as ET
-from array import *
 
-xmlFile = "sampleA.xml"
-xmlFile2 = "sampleB.xml"
 
-treeA = ET.parse(xmlFile) #xml to tree
-rootA = treeA.getroot()
-treeB = ET.parse(xmlFile2) #xml to tree
-rootB = treeB.getroot()
-
-def isTreeIdentical(root1,root2):
-    if (root1 == None and root2 == None):
+def isTreeIdentical(root1, root2):
+    if root1 == None and root2 == None:
         return True
-    if (root1 == None or root2 == None):
+    if root1 == None or root2 == None:
         return False
-    if (root1.tag != root2.tag):
+    if root1.tag != root2.tag:
         return False
-    if(len(root1) != len(root2)):
+    if len(root1) != len(root2):
         return False
     for i in range(0, len(root1)):
-        if(not isTreeIdentical(root1[i], root2[i])):
+        if not isTreeIdentical(root1[i], root2[i]):
             return False
     return True
 
-print(isTreeIdentical(rootA,rootB))
-print(isTreeIdentical(rootA[0],rootB[1]))
 
-def isSubTree (root1,root2):
-    #print(root1, root2)
+def isSubTree(root1, root2):
+    # print(root1, root2)
     if root1 is None:
         return True
- 
+
     if root2 is None:
         return False
- 
-    if (isTreeIdentical(root1, root2)):
+
+    if isTreeIdentical(root1, root2):
         return True
-    
+
     for i in range(0, len(root2)):
-        if(isSubTree(root1, root2[i])):
+        if isSubTree(root1, root2[i]):
             return True
-        
+
     return False
-    
-print(isSubTree(rootA,rootB))
-print(isSubTree(rootA[0],rootB))
--- main.py
+++ main.py
@@ -1,4 +1,21 @@
+from pydoc import Helper
 from tkinter import *
-root = Tk()
-while True:
-    root.mainloop()
+from helper.costCalc import *
+
+xmlFile = "sample/onlyTag/sampleA.xml"
+xmlFile2 = "sample/onlyTag/sampleB.xml"
+
+treeA = ET.parse(xmlFile)  # xml to tree
+rootA = treeA.getroot()
+treeB = ET.parse(xmlFile2)  # xml to tree
+rootB = treeB.getroot()
+
+print(isTreeIdentical(rootA, rootB))
+print(isTreeIdentical(rootA[0], rootB[1]))
+
+print(isSubTree(rootA, rootB))
+print(isSubTree(rootA[0], rootB))
+
+# root = Tk()
+# while True:
+#     root.mainloop()
