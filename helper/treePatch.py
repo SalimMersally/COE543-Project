612519cb7f8293687ffa6703f5eb8cbd9cfcce3f
Salim Al Mersally <salim.almersally@lau.edu>
2022-03-16 19:10:44 +0200

    Added method to edit a tree

---
+++ helper/treePatch.py
@@ -0,0 +1,45 @@
+import xml.etree.ElementTree as ET
+from copy import deepcopy
+
+
+def findSubTree(root, subTreeName):
+    subTreeRoot = root
+
+    if len(subTreeName) > 1:
+        indices = subTreeName[2 : len(subTreeName)].split("-")
+        for index in indices:
+            subTreeRoot = subTreeRoot[int(index)]
+
+    return subTreeRoot
+
+
+def findSubTreeParent(root, subTreeName):
+    subTreeRoot = root
+
+    if len(subTreeName) > 1:
+        indices = subTreeName[2 : len(subTreeName)].split("-")
+        for index in indices[0 : len(indices) - 1]:
+            subTreeRoot = subTreeRoot[int(index)]
+
+    return subTreeRoot
+
+
+def insertSubTree(A, B, rootAName, subTreeBName, position):
+    root = findSubTree(A, rootAName)
+    subTree = findSubTree(B, subTreeBName)
+    copySubTree = deepcopy(subTree)
+
+    root.insert(position, copySubTree)
+
+
+def deleteSubTree(A, subTreeAName):
+    subTreeA = findSubTree(A, subTreeAName)
+    parent = findSubTreeParent(A, subTreeAName)
+    parent.remove(subTreeA)
+
+
+def updateTag(A, B, subTreeAName, subTreeBName):
+    subTreeA = findSubTree(A, subTreeAName)
+    subTreeB = findSubTree(B, subTreeBName)
+    print(subTreeA.tag, subTreeB.tag)
+    subTreeA.tag = subTreeB.tag
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from pydoc import Helper
 from tkinter import *
 from helper.costCalc import *
 from helper.N_J import *
+from helper.treePatch import *
 
 xmlFile = "sample/onlyTag/sampleA.xml"
 xmlFile2 = "sample/onlyTag/sampleB.xml"
@@ -14,8 +14,18 @@ treeB = ET.parse(xmlFile2)  # xml to tree
 rootB = treeB.getroot()
 treeC = ET.parse(xmlFile3)  # xml to tree
 rootC = treeC.getroot()
-len(rootA)
-print(NJ(rootA,rootB))
+# len(rootA)
+# print(NJ(rootA, rootB))
+
+# insertSubTree(rootA, rootB, "A-0-1", "B", 0)
+# ET.ElementTree(rootA).write("a.xml")
+
+# deleteSubTree(rootA, "A-0-1-0")
+# ET.ElementTree(rootA).write("b.xml")
+
+# updateTag(rootA, rootB, "A-0-1", "B-0")
+# ET.ElementTree(rootA).write("c.xml")
+
 # root = Tk()
 # while True:
 #     root.mainloop()
