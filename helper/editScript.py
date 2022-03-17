164199e95a8cba4b32949d35a26c3fe7721c2d33
Ahmad Sabsabi <ahmadalsabsabii@gmail.com>
2022-03-17 17:25:56 +0200

    the modified version of editScript method // not the final version

--- helper/editScript.py
+++ helper/editScript.py
@@ -1,35 +1,58 @@
-from costCalc import *
-script = ""
+from helper.costCalc import *
+from helper.treePatch import *
+#dictionary example
+print('Try Dictionar')
+dict1 = {
+    "AB" : [[0,4,5],[1,4,4],[2,3,4]],
+    "A-1B-1" : [[1,2],[2,3],[3,4]],
+    "A-1-1B-1-1" : [[1,2,3]],
+    "A-1-2B-1-1" : [[1,2,3]],
+    "A-1B-2" : [[0,1,2],[1,0,1],[2,1,0]],
+    "A-1-1B-2-1" : [[0]],
+    "A-1-1B-2-2" : [[1]],
+    "A-1-2B-2-1" : [[1]],
+    "A-1-2B-2-2" : [[0]],
+    "A-2B-1-1" : [[1,2]],
+    "A-2B-2" : [[1,2,3]]
+}
 
-def getEditScript(dic , A, B):
-    strA = A + ''
-    strB = B + ''
+
+#End of the example 
+
+def getEditScript(dic , A, B, strA, strB):
+    
     keyDic = strA + strB
+    # ! ! ! 
     toReturn = []
     matrix = dic.get(keyDic)
 
     row = len(matrix) -1
     col = len(matrix[0])  -1
-    ############add condition for matrix 1 of 1 
+    ############add condition for matrix 1 of 1 ###############
 
     while row>0 and col>0:
-        if matrix[row][col] == matrix[row][col -1 ] + costDelete(A + row, B + col):
+        print("1")
+        if matrix[row][col] == (matrix[row - 1][col] + costDelete(findSubTree(A,(strA +"-"+ str(row))) ,findSubTree( B,(strB +"-"+ str(col))))):
             toReturn.append("DelTree("+ strA +"-"+ row + ")")
-        elif matrix[row][col] == matrix[row - 1][col] + costInsert(A + row, B + col):
+            row = row - 1
+        elif matrix[row][col] == matrix[row][col - 1] + costInsert(findSubTree(A,(strA +"-"+ str(row))) ,findSubTree( B,(strB +"-"+ str(col)))):
             toReturn.append("InsTree("+ strB +"-"+ col + ")")
+            col = col - 1
         else:
-            strA = strA + "-" + row
-            strB = strB + "-" + col
+            subtreeA = strA + "-" + row
+            subtreeB = strB + "-" + col
             # Here i need to fix the array to return 
             # else my output will have the below form
             # [Deltree#, [Deltree#,Instree#],Upd(A,B)]
-            toReturn.append(getEditScript(dic , strA, strB))
-        row = row - 1
-        col = col - 1
+            toReturn.append(getEditScript(dic , A,B,subtreeA, subtreeB))
+            row = row - 1
+            col = col - 1
+        # Here i guess it should be or so when we have the case of 0-0 it will check for update 
+        # ! ! !
         if row == 0 and col == 0:
             if matrix[row][col] == 1:
                 toReturn.append("Upd(" + strA + ","+strB)
-        # end of first while loop
+    # end of first while loop
     
     while row > 0 :
         toReturn.append("DelTree("+ strA +"-"+ row + ")")
@@ -37,15 +60,16 @@ def getEditScript(dic , A, B):
         if row == 0 and col == 0:
             if matrix[row][col] == 1:
                 toReturn.append("Upd(" + strA + ","+strB)
-        # end of the second loop
+    # end of the second loop
     
     while col > 0 :
-        toReturn.append("InsTree("+ strA +"-"+ col + ")")
+        toReturn.append("InsTree("+ strB +"-"+ col + ")")
         col = col - 1
         if row == 0 and col == 0:
             if matrix[row][col] == 1:
                 toReturn.append("Upd(" + strA + ","+strB)
-        # end of the third loop    
+    # end of the third loop    
 
 
     return toReturn
+
--- main.py
+++ main.py
@@ -2,6 +2,20 @@ from tkinter import *
 from helper.costCalc import *
 from helper.N_J import *
 from helper.treePatch import *
+from helper.editScript import *
+dict1 = {
+    "AB" : [[0,4,5],[1,4,4],[2,3,4]],
+    "A-1B-1" : [[1,2],[2,3],[3,4]],
+    "A-1-1B-1-1" : [[1,2,3]],
+    "A-1-2B-1-1" : [[1,2,3]],
+    "A-1B-2" : [[0,1,2],[1,0,1],[2,1,0]],
+    "A-1-1B-2-1" : [[0]],
+    "A-1-1B-2-2" : [[1]],
+    "A-1-2B-2-1" : [[1]],
+    "A-1-2B-2-2" : [[0]],
+    "A-2B-1-1" : [[1,2]],
+    "A-2B-2" : [[1,2,3]]
+}
 
 xmlFile = "sample/onlyTag/sampleA.xml"
 xmlFile2 = "sample/onlyTag/sampleB.xml"
@@ -14,6 +28,9 @@ treeB = ET.parse(xmlFile2)  # xml to tree
 rootB = treeB.getroot()
 treeC = ET.parse(xmlFile3)  # xml to tree
 rootC = treeC.getroot()
+
+print(getEditScript(dict1,rootA,rootB,"A","B"))
+
 # len(rootA)
 # print(NJ(rootA, rootB))
 
