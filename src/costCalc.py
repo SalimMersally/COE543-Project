a60abf03c8c2de1fc9a9fb4a60debdd6c39fcf47
Lamis Armoush <lamis.armoush@hotmail.com>
2022-03-20 02:09:16 +0200

    try1 for attributes

--- src/costCalc.py
+++ src/costCalc.py
@@ -172,3 +172,106 @@ def costUpd_TagAndText(rootA, rootB, nameA, nameB, matricesDic):
         cost += distanceOfText[len(textA.split())][len(textB.split())]
 
     return cost
+
+
+# The following method are used to calculate the cost of operation on trees
+# these method will take into consideration all Tags, attributes, and text
+# the cost model for it is:
+#   - DelTree = 1 if subtree available in B, else sum of nodes + words
+#   - InsTree = 1 if subtree available in A, else sum of nodes + words
+#   - UpdTag = 1 if tags are diffent, 0 if same + cost of upd text    
+
+def isTreeIdentical(root1, root2):
+    if root1 == None and root2 == None:
+        return True
+    if root1 == None or root2 == None:
+        return False
+    if root1.tag != root2.tag:
+        return False
+    if root1.text != root2.text:
+        return False
+    if root1.attrib != root2.attrib:
+        return False
+    if len(root1) != len(root2):
+        return False
+    for i in range(0, len(root1)):
+        if not isTreeIdentical(root1[i], root2[i]):
+            return False
+    return True
+
+
+def isSubTree(root1, root2):
+    if root1 is None:
+        return True
+
+    if root2 is None:
+        return False
+
+    if isTreeIdentical(root1, root2):
+        return True
+
+    for i in range(0, len(root2)):
+        if isSubTree(root1, root2[i]):
+            return True
+
+    return False
+
+##needs to be changed to account for cost input: an approach can include 
+##adding inputs to the functions (cost for root, tag, attribute) --> those can change depending on methods
+def nodeCounter(root):
+    counter = 0
+
+    if root is None:
+        return counter
+
+    if root.text is not None:
+        counter += 1 + len(root.text.split())
+    if root.attrib is not None:
+        counter += 1 + len(root.attrib.keys()) + len(root.attrib.values())
+    for child in root:
+        counter += nodeCounter(child)
+
+    return counter
+
+
+def costInsert(subTreeB, A):
+    if isSubTree(subTreeB, A):
+        return 1
+    else:
+        return nodeCounter(subTreeB)
+
+
+def costDelete(subTreeA, B):
+    if isSubTree(subTreeA, B):
+        return 1
+    else:
+        return nodeCounter(subTreeA)
+
+
+def costUpd(rootA, rootB, nameA, nameB, matricesDic):
+    cost = 0
+
+    if rootA.tag != rootB.tag:
+        cost += 1
+
+    if (rootA.text is not None) and (rootB.text is not None):
+        textA = rootA.text.strip()
+        textB = rootB.text.strip()
+        distanceOfText = WF(textA, textB)
+        matricesDic[nameA + "/" + nameB + "/text"] = distanceOfText
+        cost += distanceOfText[len(textA.split())][len(textB.split())]
+        
+    if (rootA.attrib is not None) and (rootB.attrib is not None):
+        keysA = rootA.attrib.keys().join(" ")
+        keysB = rootB.attrib.keys().join(" ")
+        valuesA = rootA.attrib.values().join(" ")
+        valuesB = rootB.attrib.values().join(" ")
+        
+        distKeys = WF(keysA,keysB)
+        distValues = WF(valuesA,valuesB)
+        distanceOfAttributes = distKeys[len(keysA.split())][len(keysB.split())] + distValues[len(valuesA.split())][len(valuesB.split())]
+        matricesDic[nameA + "/" + nameB + "/attribute"] = distanceOfText
+        cost+= distanceOfAttributes
+        
+
+    return cost
--- src/treeEditDistance.py
+++ src/treeEditDistance.py
@@ -96,3 +96,50 @@ def NJ_TagAndText(A, B, nameA, nameB, matricesDic):
 
     matricesDic[nameA + "/" + nameB] = Distance
     return Distance[M][N]
+
+# the following method will calculate the Edit Distance Matrices
+# to transform treeA into treeB following Nermin and Jagadish algorithm
+# this method take into considiration only tag attributes and text
+
+def NJ(A, B, nameA, nameB, matricesDic):
+    
+    subTreeA = findSubTree(A, nameA)
+    subTreeB = findSubTree(B, nameB)
+
+    M = len(subTreeA)  # number of first Level children in A
+    N = len(subTreeB)  # number of first Level children in B
+
+    Distance = [[None for i in range(N + 1)] for i in range(M + 1)]
+    Distance[0][0] = costUpd(subTreeA, subTreeB, nameA, nameB, matricesDic)
+
+    i = 1
+    j = 1
+    for childA in subTreeA:
+        Distance[i][0] = Distance[i - 1][0] + costDelete(childA, B)
+        i += 1
+
+    for childB in subTreeB:
+        Distance[0][j] = Distance[0][j - 1] + costInsert(childB, A)
+        j += 1
+
+    i = 1
+    for childA in subTreeA:
+
+        j = 1
+        for childB in subTreeB:
+            childAName = nameA + "-" + str(i - 1)
+            childBName = nameB + "-" + str(j - 1)
+
+            Distance[i][j] = min(
+                (
+                    Distance[i - 1][j - 1]
+                    + NJ(A, B, childAName, childBName, matricesDic)
+                ),
+                (Distance[i - 1][j] + costDelete(childA, B)),
+                (Distance[i][j - 1] + costInsert(childB, A)),
+            )
+            j += 1
+        i += 1
+
+    matricesDic[nameA + "/" + nameB] = Distance
+    return Distance[M][N]
